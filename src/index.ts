eeb5200a8469fa9d97e337c04f1b4e90434b69f2
Andre Restivo <andre.restivo@gmail.com>
2020-04-03 11:30:14 +0100

    Growth

--- src/index.ts
+++ src/index.ts
@@ -57,15 +57,33 @@ const orderByTotal = (countries: Map<string, number[]>) => {
   return new Map(Array.from(countries.entries()).sort((a : [string, number[]], b : [string, number[]]) => b[1][b[1].length - 1] - a[1][a[1].length - 1]))  
 }
 
-const calculateDaily = (countries: Map<string, number[]>) => {
+const calculateDaily = (total: Map<string, number[]>) => {
   const daily : Map<string, number[]> = new Map
 
-  Array.from(countries.keys()).forEach(country => {
-    const values = countries.get(country) || []
+  Array.from(total.keys()).forEach(country => {
+    const values = total.get(country) || []
     daily.set(country, values.map((value, idx) => idx == 0 ? 0 : value - values[idx - 1]))
   })
 
   return daily
+}
+
+const calculateGrowth = (daily: Map<string, number[]>) => {
+  if (!document.location.href.endsWith('#growth')) return daily
+
+  const growth : Map<string, number[]> = new Map
+
+  Array.from(daily.keys()).forEach(country => {
+    const values = daily.get(country) || []
+    let total = 0
+    growth.set(country, values.map((value) => {
+      const last = total
+      total += value
+      return last == 0 ? 0 : value / last
+    }))
+  })
+
+  return growth
 }
 
 const calculateColors = (countries: Map<string, number[]>) => {
@@ -76,8 +94,6 @@ const calculateColors = (countries: Map<string, number[]>) => {
     const max = Math.max(...values)
     colors.set(country, values.map(value => HSLToRGB(200, 100, 25 + (value / max) * 35)))
   })
-
-  console.log(colors.get('Italy'))
 
   return colors
 }
@@ -99,8 +115,8 @@ const createBar = (country: string, values: string[]) => {
   values.forEach((value, idx) => {
     const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect")
     rect.setAttribute('fill', `${value}`)
-    rect.setAttribute('x', `${idx * (1000 / values.length)}`)
-    rect.setAttribute('width', `${1000 / values.length}`)
+    rect.setAttribute('x', `${Math.floor(idx * 1000 / values.length)}`)
+    rect.setAttribute('width', `${Math.ceil(1000 / values.length) + 1}`)
     rect.setAttribute('height', '30')
     g.appendChild(rect)
   })
@@ -124,6 +140,7 @@ const loadCSV = () => {
     .then(aggregateData)
     .then(orderByTotal)
     .then(calculateDaily)
+    .then(calculateGrowth)
     .then(calculateColors)
     .then(createBars)
 }
